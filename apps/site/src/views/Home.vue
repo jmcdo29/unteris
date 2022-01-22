444e1e6c3f1cf071cbc77a81c82f0cc61e0de933
Jay McDoniel <jmcdo29@gmail.com>
2022-01-22 13:08:44 -0800

    chore: run prettier and finish dev tool setup

--- apps/site/src/views/Home.vue
+++ apps/site/src/views/Home.vue
@@ -7,14 +7,14 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import Component from 'vue-class-component'
+import Component from 'vue-class-component';
 import HelloPage from '../components/HelloWorld.vue';
 
 @Component({
   name: 'Home',
   components: {
-    HelloWorld: HelloPage
-  }
+    HelloWorld: HelloPage,
+  },
 })
 export default class HomePage extends Vue {}
 </script>
