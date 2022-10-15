8dd53044bdcd3d6991d19acfb735baa16abcb25a
Jay McDoniel <jmcdo29@gmail.com>
2022-10-15 16:45:39 -0700

    chore: remove vue

--- apps/site/src/deities/deity.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Deity {
-  name: string;
-  title: string;
-  domain: string[];
-  symbol: string;
-  alignment: string;
-}
--- libs/components/src/index.ts
+++
@@ -1,6 +0,0 @@
-import HelloWorld from './lib/HelloWorld.vue';
-import BaseAttribute from './lib/Attribute.component.vue';
-import BaseCard from './lib/Card.component.vue';
-import UnterisImage from './lib/UnterisImage.component.vue';
-import BaseGrid from './lib/Grid.component.vue';
-export { HelloWorld, BaseAttribute, BaseCard, UnterisImage, BaseGrid };
