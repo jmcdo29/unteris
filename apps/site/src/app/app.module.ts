9d081f1991d7de45e34e772705dfdd1820c5639c
Jay McDoniel <jmcdo29@gmail.com>
2022-10-22 16:49:44 -0700

    feat: set up and test styles

--- apps/site/src/app/app.module.ts
+++ apps/site/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    MatSliderModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
