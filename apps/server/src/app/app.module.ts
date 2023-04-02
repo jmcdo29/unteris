fae74d9937acc1f0dd2104ac6e1a5256f7a83998
Jay McDoniel <jmcdo29@gmail.com>
2023-04-01 19:11:42 -0700

    feat: integrate ogma as the main logger

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { OgmaModule, OgmaInterceptor } from '@ogma/nestjs-module';
+import { ExpressParser } from '@ogma/platform-express';
 import { ServerDeitiesModule } from '@unteris/server/deities';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ServerDeitiesModule],
+  imports: [
+    ServerDeitiesModule,
+    OgmaModule.forRoot({
+      service: {
+        application: 'Unteris Server',
+        logLevel: 'ALL',
+      },
+      interceptor: {
+        http: ExpressParser,
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: OgmaInterceptor,
+    },
+  ],
 })
 export class AppModule {}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,13 +5,15 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { OgmaService } from '@ogma/nestjs-module';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useLogger(app.get(OgmaService));
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
