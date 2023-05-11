4424d62eb182e77889e57fd4758aab7a910fe578
Jay McDoniel <jmcdo29@gmail.com>
2023-05-11 10:09:04 -0700

    feat: add nest-cookies for cookie support

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ServerCsrfModule } from '@unteris/server/csrf';
 import { ServerDeitiesModule } from '@unteris/server/deities';
 import { ServerLocationModule } from '@unteris/server/location';
 import { ServerLoggingModule } from '@unteris/server/logging';
+import { ServerSessionModule } from '@unteris/server/session';
+import { CookieModule, CookiesInterceptor } from 'nest-cookies';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    CookieModule,
     ServerDeitiesModule,
     ServerLocationModule,
     ServerLoggingModule.forApplication('Unteris Server', 'DEBUG'),
+    ServerCsrfModule,
+    ServerSessionModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CookiesInterceptor,
+    },
+  ],
 })
 export class AppModule {}
