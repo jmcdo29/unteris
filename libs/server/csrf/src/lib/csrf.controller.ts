1305f77f6880543fe82d96fbb0214fb2773bf238
Jay McDoniel <jmcdo29@gmail.com>
2023-05-11 10:09:30 -0700

    feat: create token creation for csrf

--- libs/server/csrf/src/lib/csrf.controller.ts
+++ libs/server/csrf/src/lib/csrf.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ServerCsrfService } from './csrf.service';
 
 @Controller('csrf')
 export class ServerCsrfController {
   constructor(private serverCsrfService: ServerCsrfService) {}
+  @Get()
+  async getCsrfToken(): Promise<string> {
+    return this.serverCsrfService.generateToken();
+  }
 }
--- libs/server/csrf/src/lib/csrf.service.ts
+++ libs/server/csrf/src/lib/csrf.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { generateKey } from 'crypto';
 
 @Injectable()
-export class ServerCsrfService {}
+export class ServerCsrfService {
+  async generateToken(): Promise<string> {
+    return new Promise((resolve, reject) =>
+      generateKey('aes', { length: 256 }, (err, key) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(key.export().toString('hex'));
+      })
+    );
+  }
+}
