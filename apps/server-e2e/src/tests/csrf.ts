abea675ad107b67b8a7054c5f01f2dd1c130296e
Jay McDoniel <jmcdo29@gmail.com>
2023-06-30 08:38:15 -0700

    test: switch server-e2e to use vitest instead of uvu
    vitest has better general support, uvu seems to be unmaintained, and
    vitest support using a separate file for e2e describe suites, meaning
    I can have better code separation for how these tests should be written
    and maintained, win-win.

--- apps/server-e2e/src/tests/csrf.ts
+++ apps/server-e2e/src/tests/csrf.ts
@@ -1,53 +1,54 @@
 import { parse } from 'lightcookie';
 import { spec } from 'pactum';
-import { Callback } from 'uvu';
-import { ok, unreachable } from 'uvu/assert';
+import { describe, expect, test } from 'vitest';
 
-export const csrfTests: Callback<any> = async (_context: any) => {
-  await spec()
-    .get('/csrf')
-    .expectStatus(200)
-    .expectJsonLike({ csrfToken: /\w+/ })
-    .expect(({ res }) => {
-      const cookies = res.headers['set-cookie'];
-      if (!cookies || cookies.length === 0) {
-        unreachable('Received no cookies from the server');
-        return;
-      }
-      cookies.forEach((cookie) => {
-        const parsed = parse(cookie);
-        ok(
-          ['sessionId', 'refreshId'].some((cookieName) => {
-            return cookieName in parsed;
-          })
-        );
-      });
-    })
-    .stores((_req, res) => {
-      const { csrfToken } = res.body;
-      const cookies = res.headers['set-cookie'];
-      if (!cookies || cookies.length === 0) {
-        unreachable('Received no cookies from the server');
-        return;
-      }
-      const sessionId = cookies
-        .map((c) => parse(c))
-        .find((cookie) => 'sessionId' in cookie)?.sessionId;
-      const refreshId = cookies
-        .map((c) => parse(c))
-        .find((cookie) => 'refreshId' in cookie)?.refreshId;
-      return {
-        csrfToken,
-        sessionId,
-        refreshId,
-      };
-    })
-    .toss();
-  await spec()
-    .post('/csrf/verify')
-    .withHeaders('X-UNTERIS-CSRF-PROTECTION', '$S{csrfToken}')
-    .withCookies('sessionId', '$S{sessionId}')
-    .expectStatus(201)
-    .expectJson({ success: true })
-    .toss();
+export const csrfTest = () => {
+  return describe('CSRF Tests', () => {
+    test('CSRF Testing', async () => {
+      await spec()
+        .get('/csrf')
+        .expectStatus(200)
+        .expectJsonLike({ csrfToken: /\w+/ })
+        .expect(({ res }) => {
+          const cookies = res.headers['set-cookie'];
+          if (!cookies || cookies.length === 0) {
+            throw new Error('Received no cookies from the server');
+          }
+          cookies.forEach((cookie) => {
+            const parsed = parse(cookie);
+            expect(
+              ['sessionId', 'refreshId'].some((cookieName) => {
+                return cookieName in parsed;
+              })
+            );
+          });
+        })
+        .stores((_req, res) => {
+          const { csrfToken } = res.body;
+          const cookies = res.headers['set-cookie'];
+          if (!cookies || cookies.length === 0) {
+            throw new Error('Received no cookies from the server');
+          }
+          const sessionId = cookies
+            .map((c) => parse(c))
+            .find((cookie) => 'sessionId' in cookie)?.sessionId;
+          const refreshId = cookies
+            .map((c) => parse(c))
+            .find((cookie) => 'refreshId' in cookie)?.refreshId;
+          return {
+            csrfToken,
+            sessionId,
+            refreshId,
+          };
+        })
+        .toss();
+      await spec()
+        .post('/csrf/verify')
+        .withHeaders('X-UNTERIS-CSRF-PROTECTION', '$S{csrfToken}')
+        .withCookies('sessionId', '$S{sessionId}')
+        .expectStatus(201)
+        .expectJson({ success: true })
+        .toss();
+    });
+  });
 };
