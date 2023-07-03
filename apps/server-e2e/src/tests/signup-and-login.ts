fc1ad2ac81d1b0b7c9326d1d71cfe492d4b2fa31
Jay McDoniel <jmcdo29@gmail.com>
2023-07-03 13:47:51 -0700

    feat: run migrations before e2e tests and update signup test

--- apps/server-e2e/src/tests/signup-and-login.ts
+++ apps/server-e2e/src/tests/signup-and-login.ts
@@ -1,4 +1,5 @@
-import { base32 } from '@unteris/shared/base32';
+import { base32Regex } from '@unteris/shared/base32';
+import { randomUUID } from 'crypto';
 import { spec } from 'pactum';
 import { regex } from 'pactum-matchers';
 import { describe, expect, test } from 'vitest';
@@ -7,33 +8,34 @@ import { DbContext } from '../interfaces/test-context.interface';
 export const signUpAndLoginTests = () => {
   return describe('SignUp and Login', () => {
     test<DbContext>('A new user should be able to sign up', async (context) => {
+      const email = `${randomUUID()}@testing.com`;
       const res = await spec()
         .post('/auth/signup')
         .withBody({
-          email: 'test@testing.com',
+          email,
           password: 'ALongEnoughP4ssw0rdToBeFin3',
           confirmationPassword: 'ALongEnoughP4ssw0rdToBeFin3',
-          name: 'Test User',
+          name: 'Test User' + randomUUID(),
         })
         .expectStatus(201)
         .expectJsonMatch({
-          id: regex(base32),
+          id: regex(base32Regex),
           success: true,
         })
         .returns('.id');
       // assert we properly made the user, login method, and related local login
       const userAccount = await context.db
         .selectFrom('userAccount as ua')
-        .innerJoin('loginMethod as lm', 'lm.id', 'ua.id')
+        .innerJoin('loginMethod as lm', 'lm.userId', 'ua.id')
         .innerJoin('localLogin as ll', 'll.loginMethodId', 'lm.id')
         .select(({ fn }) => [fn.count('ua.id').as('count')])
         .where('ua.id', '=', res)
         .execute();
-      expect(userAccount[0].count).toBe(1);
+      expect(userAccount[0].count).toBe('1');
       await spec()
-        .post('auth/login')
+        .post('/auth/login')
         .withBody({
-          email: 'test@testing.com',
+          email,
           password: 'ALongEnoughP4ssw0rdToBeFin3',
         })
         .expectJson({
