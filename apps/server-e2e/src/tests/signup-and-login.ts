544ff3611cf38ca8eb4ff26a00483f9c649486d0
Jay McDoniel <jmcdo29@gmail.com>
2023-07-02 19:19:08 -0700

    test: add check for database rows in sign up test

--- apps/server-e2e/src/tests/signup-and-login.ts
+++ apps/server-e2e/src/tests/signup-and-login.ts
@@ -1,13 +1,13 @@
 import { base32 } from '@unteris/shared/base32';
 import { spec } from 'pactum';
 import { regex } from 'pactum-matchers';
-import { describe, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { DbContext } from '../interfaces/test-context.interface';
 
 export const signUpAndLoginTests = () => {
   return describe('SignUp and Login', () => {
-    test<DbContext>('A new user should be able to sign up', async (_context) => {
-      await spec()
+    test<DbContext>('A new user should be able to sign up', async (context) => {
+      const res = await spec()
         .post('/auth/signup')
         .withBody({
           email: 'test@testing.com',
@@ -15,11 +15,21 @@ export const signUpAndLoginTests = () => {
           confirmationPassword: 'ALongEnoughP4ssw0rdToBeFin3',
           name: 'Test User',
         })
+        .expectStatus(201)
         .expectJsonMatch({
           id: regex(base32),
           success: true,
         })
-        .toss();
+        .returns('.id');
+      // assert we properly made the user, login method, and related local login
+      const userAccount = await context.db
+        .selectFrom('userAccount as ua')
+        .innerJoin('loginMethod as lm', 'lm.id', 'ua.id')
+        .innerJoin('localLogin as ll', 'll.loginMethodId', 'lm.id')
+        .select(({ fn }) => [fn.count('ua.id').as('count')])
+        .where('ua.id', '=', res)
+        .execute();
+      expect(userAccount[0].count).toBe(1);
       await spec()
         .post('auth/login')
         .withBody({
@@ -32,8 +42,7 @@ export const signUpAndLoginTests = () => {
         .expectStatus(201)
         .expect(({ res: _res }) => {
           // assert session token here
-        })
-        .toss();
+        });
     });
   });
 };
