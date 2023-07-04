31699333aa42082fd8db632cbe568e59baaf549d
Jay McDoniel <jmcdo29@gmail.com>
2023-07-04 10:52:28 -0700

    feat: set up sign up and login paths

--- libs/server/security/src/lib/security.controller.ts
+++ libs/server/security/src/lib/security.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Session } from '@nestjs/common';
+import { SavedSessionData } from '@unteris/server/session';
+import { LoginBodyDto, SignupBody } from '@unteris/shared/types';
 import { ServerSecurityService } from './security.service';
 
 @Controller('auth')
@@ -6,12 +8,20 @@ export class ServerSecurityController {
   constructor(private serverSecurityService: ServerSecurityService) {}
 
   @Post('signup')
-  async signup() {
-    return {};
+  async signup(
+    @Body() body: SignupBody,
+    @Session() session: { id: string & SavedSessionData }
+  ) {
+    console.log(session);
+    return this.serverSecurityService.signUpLocal(body.data, session.id);
   }
 
   @Post('login')
-  async login() {
-    return {};
+  async login(
+    @Body() body: LoginBodyDto,
+    @Session() session: SavedSessionData
+  ) {
+    console.log(session);
+    return this.serverSecurityService.logUserIn(body.data);
   }
 }
--- libs/server/security/src/lib/security.module.ts
+++ libs/server/security/src/lib/security.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ServerHashModule } from '@unteris/server/hash';
+import { KyselyModule } from '@unteris/server/kysely';
+import { ServerSessionModule } from '@unteris/server/session';
 import { ServerSecurityController } from './security.controller';
 import { ServerSecurityService } from './security.service';
 
 @Module({
+  imports: [KyselyModule, ServerSessionModule, ServerHashModule],
   controllers: [ServerSecurityController],
   providers: [ServerSecurityService],
   exports: [ServerSecurityService],
