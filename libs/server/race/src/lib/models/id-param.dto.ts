8cf95cae6d1e11e65fb9b1f5c6da619c202eb285
Jay McDoniel <jmcdo29@gmail.com>
2023-07-04 15:13:24 -0700

    fix: create schema and dto for race controller

---
+++ libs/server/race/src/lib/models/id-param.dto.ts
@@ -0,0 +1,6 @@
+import { ZodDtoClass } from '@unteris/server/zod-pipe';
+import { IdParamSchema } from '@unteris/shared/types';
+
+export class IdParamDto extends ZodDtoClass<typeof IdParamSchema> {
+  static override schema = IdParamSchema;
+}
