d308a1e37ab7ce14b0ed3beab27996cf1a8d65a7
Jay McDoniel <jmcdo29@gmail.com>
2023-07-29 19:54:25 -0700

    feat: email verificaiton page
    Wording probably needs some updating, but it'll work for now.
    The User page has also been moved to a separate route rather than a part
    of login, and the profile button could probably be made to account
    for that along with having the login page automatically redirect in
    the case of already being signed in.
    Lastly, a new fetch helper has been added `getFetch`, for simple get
    requests. This is to be used with async atoms from jotai.

--- libs/ui/auth/src/lib/ui-auth.tsx
+++ libs/ui/auth/src/lib/ui-auth.tsx
@@ -2,15 +2,20 @@ import { useTheme } from '@mui/material';
 import { userAtom } from '@unteris/ui/atoms';
 import { Grid } from '@unteris/ui/components';
 import { useAtomValue } from 'jotai';
+import { useNavigate } from 'react-router-dom';
 import { LoginOrRegister } from './login-or-register';
-import { User } from './user';
 
 export function UiAuth() {
   const theme = useTheme();
   const user = useAtomValue(userAtom);
+  const nav = useNavigate();
+  if (user.id) {
+    nav('/me');
+    return <div />;
+  }
   return (
     <Grid columns={1} sx={{ justifyItems: 'center', rowGap: theme.spacing(2) }}>
-      {user.id ? <User user={user} /> : <LoginOrRegister />}
+      <LoginOrRegister />
     </Grid>
   );
 }
