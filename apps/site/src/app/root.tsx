6ebb7befbfacda284123fbaed45266a64e1463e0
Jay McDoniel <jmcdo29@gmail.com>
2023-08-05 09:50:57 -0700

    feat: create error boundary for router

--- apps/site/src/app/root.tsx
+++ apps/site/src/app/root.tsx
@@ -1,15 +1,24 @@
 import { Box, useTheme } from '@mui/material';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Footer } from './footer';
 import { NavBar } from './nav-bar';
 
-export const Root = (): JSX.Element => {
+export const Root = ({
+  children,
+}: {
+  children?: React.ReactNode;
+}): JSX.Element => {
   const theme = useTheme();
+  console.log(children);
   return (
     <>
       <NavBar />
       <Box minHeight="85vh" padding={`${theme.spacing(3)} ${theme.spacing(2)}`}>
-        <Outlet />
+        <>
+          <Outlet />
+          {children ? children : <div />}
+        </>
       </Box>
       <Footer />
     </>
--- apps/site/src/app/router.tsx
+++ apps/site/src/app/router.tsx
@@ -1,22 +1,64 @@
-import { createBrowserRouter, RouteObject } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouteObject,
+  useRouteError,
+} from 'react-router-dom';
 import {
   EmailVerification,
   PasswordReset,
   UiAuth,
   User,
 } from '@unteris/ui/auth';
+import { Heading } from '@unteris/ui/components';
 import { DeityNav } from '@unteris/ui/deities';
 import { History } from '@unteris/ui/history';
 import { UiRace } from '@unteris/ui/race';
 import { Root } from './root';
 
 import { Welcome } from './welcome/welcome';
+import { Box, useTheme } from '@mui/material';
+
+const ErrorBoundary = (): JSX.Element => {
+  const error = useRouteError();
+  const theme = useTheme();
+  if (error instanceof Error) {
+    return (
+      <Root>
+        <>
+          <Heading text={error.name} />
+          <details>
+            <summary>{error.message}</summary>
+            <Box
+              sx={{
+                fontFamily: 'monospace',
+                margin: `0 ${theme.spacing(3)}`,
+                backgroundColor: theme.palette.grey[700],
+              }}
+            >
+              {error.stack}
+            </Box>
+          </details>
+        </>
+      </Root>
+    );
+  } else {
+    return (
+      <Root>
+        <>
+          <Heading text="Unknown Error" />
+          <div>{`${error}`}</div>
+        </>
+      </Root>
+    );
+  }
+};
 
 const routes = (): RouteObject[] => {
   return [
     {
       path: '/',
       element: <Root />,
+      errorElement: <ErrorBoundary />,
       children: [
         { index: true, element: <Welcome /> },
 
