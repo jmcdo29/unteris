4645b5825af1b2563886fbc06c02bd8d396847fb
Jay McDoniel <jmcdo29@gmail.com>
2023-08-05 23:03:22 -0700

    feat: update auth component with atoms

--- libs/ui/auth/src/lib/user.tsx
+++ libs/ui/auth/src/lib/user.tsx
@@ -1,18 +1,13 @@
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { csrfAtom, userAtom } from '@unteris/ui/atoms';
-import { postFetch } from '@unteris/ui/components';
-import { useAtom, useAtomValue } from 'jotai';
+import { userAtom } from '@unteris/ui/atoms';
+import { sdk } from '@unteris/ui/components';
+import { useAtom } from 'jotai';
 
 export const User = (): JSX.Element => {
   const [user, setUser] = useAtom(userAtom);
-  const csrfToken = useAtomValue(csrfAtom);
   const logout = async () => {
-    await postFetch({
-      endpoint: 'auth/logout',
-      body: {},
-      csrfToken,
-    });
+    await sdk.logout();
     setUser({ id: '', email: '', displayName: '' });
   };
   return (
