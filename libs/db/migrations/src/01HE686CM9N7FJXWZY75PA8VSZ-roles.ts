e3a01c10d99b62364e7cde7baf3498adc52800fa
Jay McDoniel <jmcdo29@gmail.com>
2023-11-01 13:20:08 -0700

    chore: create the roles as a migration

---
+++ libs/db/migrations/src/01HE686CM9N7FJXWZY75PA8VSZ-roles.ts
@@ -0,0 +1,24 @@
+import { DB } from "./utils/db.interface";
+
+const roles = ["player", "dev", "dm", "admin"];
+
+export const up = async (db: DB) => {
+	await db
+		.insertInto("role")
+		.values(roles.map((r) => ({ name: r })))
+		.execute();
+};
+
+export const down = async (db: DB) => {
+	await db
+		.deleteFrom("user_permission")
+		.where((ref) =>
+			ref(
+				"role_id",
+				"in",
+				ref.selectFrom("role").select("id").where("name", "in", roles),
+			),
+		)
+		.execute();
+	await db.deleteFrom("role").where("name", "in", roles).execute();
+};
